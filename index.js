c6af09e9c6e1d8437ca2cfb37d3e03e13d54109b
MUHAMMADAIZAZ-TECH <aizaznadeem174@gmail.com>
2023-04-15 03:44:45 -0700

    origin

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(passport.session());
 //middlewares
 app.use(
   cors({
-    origin: [process.env.CLIENT_ORIGIN],
+    origin: "*",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
